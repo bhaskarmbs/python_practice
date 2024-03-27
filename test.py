e2059375976079751d5083432162d97da238dfb3
Bhaskar <bhaskarrraju.0406@gmail.com>
2024-03-27 21:24:46 +0000

    Added test file

---
+++ test.py
@@ -0,0 +1,15 @@
+print("Hi, This is Bhaskar")
+
+age = 35
+name = 'Bhaskar'
+
+print(age)
+print(name)
+print('age vaariable type is:', type(age))
+print('name variable type is:',type(name))
+
+a=b=c=55
+
+print('a variable value is:',a)
+print('b variable value is:',b)
+print('c variable value is:',c)
